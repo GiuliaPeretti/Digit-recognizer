5a7d5dcf339bd92cf13cc0f8a141886a227c61c7
GiuliaPeretti <giulia.peretti12@gmail.com>
2024-05-06 12:50:04 +0200

    disegno pixellato

--- GUI.py
+++ GUI.py
@@ -2,7 +2,6 @@ from tkinter import *
 from tkinter.ttk import *
 import numpy as np 
 import pandas as pd 
-from matplotlib import pyplot as plt
 
 # def reset():
 #     pixel=[[0]*28]*28
@@ -10,17 +9,25 @@ from matplotlib import pyplot as plt
 def draw_on_canvas(event):
     print(event.type)
     global prevPoint
-    global currntPoint
+    global currentPoint
+    global pixel
     x=event.x
     y=event.y
-    currntPoint = [x,y]
+    currentPoint = [x,y]
+    #currentPoint = [x//28, y//28]
+    square =  [x//28, y//28]
 
     if prevPoint != [0,0]: 
-        c.create_line(prevPoint[0], prevPoint[1],currntPoint[0],currntPoint[1], activewidth=100, fill="white")
+        #TODO: sistemare cooridnate matrice
+        pos=(y//28)*28+(x//28)%28
+        pixel[pos]=1
+        c.create_rectangle( square[0]*28, square[1]*28, (square[0]+1)*28, (square[1]+1)*28, fill="white", outline="white")
+        #c.create_line(prevPoint[0], prevPoint[1],currentPoint[0],currentPoint[1], activewidth=100, fill="white")
         #c.create_oval((x-brush_size/2, y-brush_size/2, x+brush_size/2, y+brush_size/2), outline="white",fill="white")
-    prevPoint=currntPoint
+    prevPoint=currentPoint
     if(event.type=="5"):
         prevPoint=[0,0]
+    print(pixel)
 
 gui=Tk()
 gui.title("Visualizer")
@@ -39,12 +46,21 @@ gui.configure(background="pink")
 #     pixel.append(p)
 brush_size=4
 prevPoint=[0,0]
-currntPoint=[0,0]
+currentPoint=[0,0]
+pixel=[0]*784
 #sistemare la dimensione
-c=Canvas(gui, bg="black", height=28, width=28, bd=100)
+c=Canvas(gui, bg="black", height=420, width=420)
+#TODO: aggiungi griglia per vedere pixel
+for i in range (0,28):
+    c.create_line(0, i*28, 420, i*28, fill="gray")
+    c.create_line(i*28, 0, i*28, 420, fill="gray")
 c.bind('<B1-Motion>', draw_on_canvas)
+#TODO: aggiungere anche se il pulsante viene solo premuto
+#TODO: aggiungere cancellare
 c.bind('<ButtonRelease-1>', draw_on_canvas)
-c.pack()
+c.grid(row=0, column=0, rowspan=100, columnspan=100)
 #c.grid(row=0, column=0, rowspan=100, columnspan=100)
+answer=Entry(gui)
+answer.grid(row=0, column=100)
         
 gui.mainloop()
